7d212f736ac8aed3f5c12e55d5adec808a3812ed
Eric Zhou <ericfzhou@berkeley.edu>
2016-12-31 02:19:27 -0800

    added jacob

--- main.js
+++ main.js
@@ -24,7 +24,8 @@ party = ['Tools', 'Glass', 'Toolss', 'bleevl', 'bleevlsss', 'AidElk',
          'Edylc', 'LeonXu', 'LeonXu2', 'LeonXu4', 'iloveyou56', 'nopo',
          'bleeeeevl'];
 
-fleeList = ['Ifrit']; // weak characters that scary people use to get us good
+// weak characters that scary people use to get us good
+fleeList = ['Ifrit', 'jacob']; 
 
 // use hpot or mpot when lacking this much
 useHP = 200;
